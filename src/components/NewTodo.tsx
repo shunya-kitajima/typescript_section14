16ea1ad32cca4b08aa295d03f960ad0bb2819c45
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-03 21:34:24 +0900

    スタイルを追加

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import './TodoList.css';
 
 type NewTodoProps = {
   onAddTodo: (todoText: string) => void
@@ -12,7 +13,7 @@ const NewTodo: React.FC<NewTodoProps> = props => {
     props.onAddTodo(enteredText);
   };
   return <form onSubmit={todoSubmitHandler}>
-    <div>
+    <div className="form-control">
       <label htmlFor="todo-text">Todo内容</label>
       <input type="text" id="todo-text" ref={textInputRef} />
     </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NewTodo.css';
 
 interface TodoListProps {
   items: { id: string; text: string }[];
