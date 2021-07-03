8c81baaec369fcae93aff9449df91623a0039cac
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-03 15:34:16 +0900

    NewTodoのコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 // function App() {
 //   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
   const todos = [{id: 't1', text: 'TypeScriptコースの完了'}];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
