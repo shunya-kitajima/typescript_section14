923aae218154736f4443ca895a053f3ae9d83943
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-03 15:52:09 +0900

    useStateを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
-
-// function App() {
-//   return (
-//     <div className="App">
-      
-//     </div>
-//   );
-// }
+import { Todo } from './components/todo.model';
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'TypeScriptコースの完了'}];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos([{id: Math.random().toString(), text: text}]);
+    console.log(text);
+  };
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
